ed83879482caab8d97903e7c0ed95858a58aa58d
mamashengde <3158442894@qq.com>
2023-01-01 22:48:50 +0800

    change2
    又是一次大修改

--- TestLast/src/com/inspur/ssm/mapper/CostsettledetailsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.inspur.ssm.mapper;
-
-import com.inspur.ssm.pojo.Costsettledetails;
-
-public interface CostsettledetailsMapper {
-    int insert(Costsettledetails record);
-
-    int insertSelective(Costsettledetails record);
-}
--- TestLast/src/com/inspur/ssm/mapper/PaymentdetailsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.inspur.ssm.mapper;
-
-import com.inspur.ssm.pojo.Paymentdetails;
-
-public interface PaymentdetailsMapper {
-    int insert(Paymentdetails record);
-
-    int insertSelective(Paymentdetails record);
-}
