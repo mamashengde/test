b9eec28564854472ce4ccf6d62dc5590858934fe
mamashengde <3158442894@qq.com>
2023-01-01 23:16:05 +0800

    change
    大修改

---
+++ SSM-Hospital/src/com/inspur/ssm/mapper/UserssmMapper.java
@@ -0,0 +1,27 @@
+package com.inspur.ssm.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.inspur.ssm.pojo.Userssm;
+
+public interface UserssmMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Userssm record);
+
+    int insertSelective(Userssm record);
+
+    Userssm selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Userssm record);
+
+    int updateByPrimaryKey(Userssm record);
+
+	Userssm findUser(Userssm userssmQuery);
+
+	// .....      
+    public List<Userssm> getUserList(Map<String,Object> params);  
+    //分页总条数  
+    public Long getCounts(Map<String,Object> p);  
+}
