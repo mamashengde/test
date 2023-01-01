d292362e67762a1897881daa577e16c9649d1df6
mamashengde <3158442894@qq.com>
2023-01-01 18:44:05 +0800

    delete
    删除

--- src/com/inspur/ssm/mapper/FriendMapper.java
+++
@@ -1,9 +0,0 @@
-package com.inspur.ssm.mapper;
-
-import com.inspur.ssm.pojo.Books;
-import com.inspur.ssm.pojo.Friends;
-
-public interface FriendMapper {
-    //增加一个朋友
-    int addFriends(Friends friend);
-}
--- src/com/inspur/ssm/mapper/UserssmMapper.java
+++
@@ -1,7 +0,0 @@
-package com.inspur.ssm.mapper;
-
-import com.inspur.ssm.pojo.Userssm;
-
-public interface UserssmMapper {
-	Userssm findUser(Userssm userssmQuery);
-}
